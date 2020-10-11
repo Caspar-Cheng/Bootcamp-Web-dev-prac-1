1f13304d9ec2d49bf87784899e411e01d4326d3b
Caspar-Cheng <52498280+Caspar-Cheng@users.noreply.github.com>
2020-10-11 11:56:33 +1000

    Add files via upload
    Second practice program: Drum Kit

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
-var randomNum1 = Math.floor(Math.random() * 6) + 1;
-var randomNum2 = Math.floor(Math.random() * 6) + 1;
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
 
-var imagePlayer1 = "images/" + "dice" + randomNum1 + ".png";
-var imagePlayer2 = "images/" + "dice" + randomNum2 + ".png";
+for (var i = 0; i < numOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-document.querySelectorAll("img")[0].setAttribute("src", imagePlayer1);
-document.querySelectorAll("img")[1].setAttribute("src", imagePlayer2);
+        var buttonOfDrum = this.innerHTML;
 
-if(randomNum1 > randomNum2){
-    document.querySelector("h1").innerHTML = "Player1 wins!";
+        clickSound(buttonOfDrum);
+
+        buttonAnimation(buttonOfDrum);
+
+    });
 }
-else if(randomNum1 < randomNum2){
-    document.querySelector("h1").innerHTML = "Player2 wins";
+
+document.addEventListener("keydown", function(event){
+
+    clickSound(event.key);
+
+    buttonAnimation(event.key);
+
+});
+
+function clickSound(key){
+
+    switch(key){
+
+        case "w":
+            var crash =  new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case "d":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "j":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "l":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        default: console.log(key);
+
+    };
 }
-else{
-    document.querySelector("h1").innerHTML = "Draw!!!";
+
+function buttonAnimation(currentKey){
+
+    var activeButton =  document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
